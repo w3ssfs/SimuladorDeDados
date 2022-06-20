ef210e3c730760d3bbd0ee639be61d267c50c811
w3ssfs <wesleyfranca001@gmail.com>
2022-06-20 20:09:39 -0300

    Adicionando interface

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
 import random
 from time import sleep
-
+import PySimpleGUI as sg
 
 
 def linha ():
     print("="*30)
 
 
+
+
+
+
+
+
+
 def dado ():
+    # Layout
+    layout = [
+        [sg.Text('Jogar Dado?')],
+        [sg.Button('sim'), sg.Button('nao')]
+    ]
+
+    # criar uma janela
+    janela = sg.Window('Simulador de Dado', layout=layout)
+    # Ler os valores
+    evento, valores = janela.Read()
+    # Fazer alguma coisa
+
 
     dados = random.randint(1,6)
     while True:
         try:
-            respo = input('Quer Jogar um Dado? \n>> ')
-            if respo == 'sim' or respo == 's':
-                print(f'Dados Girando.........')
-                sleep(1)
-                print(f'Número: {dados}')
-                break
-            elif respo == 'nao' or respo == 'n':
-                print('Obrigado, Volte sempre para jogar XD')
-                break
-            else:
-                print('Por favor digite Sim ou Nao')
-                linha()
+
+                if evento == 'sim' or evento == 's':
+                    print(f'Dados Girando.........')
+                    sleep(1)
+                    print(f'Número: {dados}')
+                    break
+                elif evento == 'nao' or evento == 'n':
+                    print('Obrigado, Volte sempre para jogar XD')
+                    break
+                else:
+                    print('Por favor digite Sim ou Nao')
+                    linha()
+                    break
         except:
-            print('ERRO! Responda com SIM ou NAO!')
+                print('ERRO! Responda com SIM ou NAO!')
 
 linha()
 print(f'\033[1;32m{"Simulador de Dados":^30}\033[m')
